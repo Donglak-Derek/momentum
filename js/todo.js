54b3789264115242e1b57c24df1d37f0299666bd
DerekHan <donglak.han@gmail.com>
2022-09-11 23:28:46 -0500

    delete toDo list w button

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,23 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+function deleteTodo(event){
+    const li = event.target.parentElement; //event, console.dir, find parentElement which is li
+    li.remove();        //each button has different parent = li
+}
+
+
 // add new todo to the list
 function paintTodo(newTodo){
     const li = document.createElement("li");    //creat <li></li>
     const span = document.createElement("span");
-    li.appendChild(span);                       //put span in li
     span.innerText = newTodo;                   // value between span
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);                       //put span in li
+    li.appendChild(button);                     // button to list
     toDoList.appendChild(li);                   // add li to html
 }
 
