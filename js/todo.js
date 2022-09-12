60d3326d3ae592839b025077a8f4bbd0f7119845
DerekHan <donglak.han@gmail.com>
2022-09-12 15:12:44 -0500

    save toDos in local storage

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos(){
+    localStorage.setItem("toDos", JSON.stringify(toDos)); //make them array!
+}
 
 function deleteTodo(event){
     const li = event.target.parentElement; //event, console.dir, find parentElement which is li
@@ -24,8 +29,10 @@ function paintTodo(newTodo){
 
 function handleToDoSubmit(event){
     event.preventDefault();
-    const newTodo = toDoInput.value;    // save current value of input
-    toDoInput.value = "";               //clear input from the window
+    const newTodo = toDoInput.value;    // save current value
+    toDoInput.value = "";  //clear
+    toDos.push(newTodo);
+    saveTodos();             
     paintTodo(newTodo);
     
 }
